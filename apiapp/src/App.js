36b78e0bf4c1c1df49343a9937ae0f3da91fc881
Aaron Badgett <aaron.badgett@outlook.com>
2024-03-15 13:48:15 -0400

    modified:   apiapp/package-lock.json 	modified:   apiapp/package.json 	modified:   apiapp/src/App.js 	modified:   apiapp/src/index.js 	deleted:    apiapp/src/logo.svg 	deleted:    apiapp/src/reportWebVitals.js 	deleted:    apiapp/src/setupTests.js

--- apiapp/src/App.js
+++ apiapp/src/App.js
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from 'react';
-
-import './App.css';
 import axios from 'axios';
-// import RecipeDisplay from './components/RandomRecipe';
 
 function App() {
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchRecipe = async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
-        params: {
-          tags: 'vegetarian',
-          number: '1'
-        },
-        headers: {
-          'X-RapidAPI-Key': '4dcf9c0544mshb812bc2bd441b4fp1c8b78jsn90580a18b7bd',
-          'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await axios.request(options);
-        setRecipe(response.data.recipes[0]);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
+  const fetchRecipe = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
+      params: {
+        tags: 'vegetarian',
+        number: '1'
+      },
+      headers: {
+        'X-RapidAPI-Key': '4dcf9c0544mshb812bc2bd441b4fp1c8b78jsn90580a18b7bd',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
       }
     };
 
+    try {
+      const response = await axios.request(options);
+      setRecipe(response.data.recipes[0]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const handleClick = () => {
+    setLoading(true);
+    fetchRecipe();
+  };
+
+  useEffect(() => {
     fetchRecipe();
   }, []);
 
@@ -43,26 +45,26 @@ function App() {
   if (!recipe) {
     return <div>No recipe found.</div>;
   }
+
   return (
     <div>
       <h1>Random Vegetarian Recipe</h1>
-        {/* <RecipeDisplay/> */}
-        return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.image} alt={recipe.title} />
-      <h2>Ingredients:</h2>
-      <ul>
-        {recipe.extendedIngredients.map((ingredient, index) => (
-          <li key={index}>{ingredient.original}</li>
-        ))}
-      </ul>
-      <h2>Instructions:</h2>
-      <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}></div>
-    </div>
+      <button onClick={handleClick}>Get Another Recipe</button>
+      <br />
+      <div>
+        <h1>{recipe.title}</h1>
+        <img src={recipe.image} alt={recipe.title} />
+        <h2>Ingredients:</h2>
+        <ul>
+          {recipe.extendedIngredients.map((ingredient, index) => (
+            <li key={index}>{ingredient.original}</li>
+          ))}
+        </ul>
+        <h2>Instructions:</h2>
+        <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}></div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
